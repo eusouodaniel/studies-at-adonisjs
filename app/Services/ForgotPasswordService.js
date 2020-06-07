c599dea51e9df864627aa834040cc670693c3201
Daniel Silva <danielrodriguesdrs331@gmail.com>
2020-06-07 11:45:34 -0300

    NEW: Send mail after request token

--- app/Services/ForgotPasswordService.js
+++ app/Services/ForgotPasswordService.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const crypto = require('crypto')
+const Mail = use('Mail')
 const User = use('App/Models/User')
 
 class ForgotPasswordService {
@@ -11,6 +12,17 @@ class ForgotPasswordService {
       user.token_created_at = new Date()
 
       await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        { email, token: user.token, link: `${request.input('redirect_url')}?token=${user.token}` },
+        message => {
+          message
+            .to(user.mail)
+            .from(Env.get('SMTP_FROM_EMAIL'), Env.get('SMTP_FROM_NAME'))
+            .subject("Recuperação de senha")
+        }
+      )
   }
 }
 
